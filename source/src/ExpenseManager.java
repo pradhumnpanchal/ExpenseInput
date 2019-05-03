bbeb4c1a771b1b390faa4d3d678a6abb14280131
pradhumnpanchal <pradhumn44@gmail.com>
2019-05-03 21:53:22 +0530

    //

--- source/src/ExpenseManager.java
+++ source/src/ExpenseManager.java
@@ -2,48 +2,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ExpenseManager implements ActionListener {
-    
     JFrame frame ;
     JTextField t1 = new JTextField("");
     JTextField t2 = new JTextField("");
-    
     void go(){
-        
         frame = new JFrame("Daily Expenses");
-        
         JLabel l = new JLabel("DAILY KA NATAK");
         JLabel l1 = new JLabel("Amount");
         JLabel l2 = new JLabel("Description");
         JButton b = new JButton("SUBMIT");
-        
         frame.add(l1);
         frame.add(l2);
         frame.add(l);
-        
         l.setForeground(Color.WHITE);
         l.setBounds(100,2,500,30);
-        
         l1.setForeground(Color.WHITE);
         l1.setBounds(50,30,500,20);
-        
         l2.setForeground(Color.WHITE);
         l2.setBounds(50,80,500,20);
-        
         frame.add(t1);
         t1.setBounds(50,50,200,30);
-        
         frame.add(t2);
         t2.setBounds(50,100,200,30);
-        
         frame.add(b);
         b.setBounds(50,150,200,30);
-        
         b.addActionListener(this);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(null);
@@ -57,21 +42,28 @@ public class ExpenseManager implements ActionListener {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/try","root","root");
-            Statement stmt=con.createStatement();
+            //Statement stmt=con.createStatement();
+            String sql = "INSERT INTO data (amt, dis) VALUES(?,?)";
             String x = t1.getText();
-            int x1 = Integer.parseInt(x);
-            String y = t2.getText();
-            stmt.executeUpdate("INSERT INTO data (amt, dis)"+" VALUES ('"+x1+"' ,'"+y+"');");
-            
+            //int x1 = Integer.parseInt(x);
+            //String y = t2.getText();
+            int x1 = 11;
+            String y = "fuck";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, x1);
+            pstmt.setString(2, y);
+            pstmt.executeUpdate();
+//            String x = t1.getText();
+//            int x1 = Integer.parseInt(x);
+//            String y = t2.getText();
+//            stmt.executeUpdate("INSERT INTO data (amt, dis)"+" VALUES ('"+x1+"' ,'"+y+"');");
             con.close();
-            stmt.close();
+            pstmt.close();
         }catch(Exception eX){ System.out.println(e);}
     }
 
     public static void main(String[] args) {
-        
         ExpenseManager e = new ExpenseManager();
         e.go();
-        
     }
 }
