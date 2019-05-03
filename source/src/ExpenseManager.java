630c7d0a8ed8721dfa8a12967bbb934204edb85b
pradhumnpanchal <pradhumn44@gmail.com>
2019-05-03 22:00:19 +0530

    finish code

--- source/src/ExpenseManager.java
+++ source/src/ExpenseManager.java
@@ -38,8 +38,10 @@ public class ExpenseManager implements ActionListener {
 
         frame.add(t2);
         t2.setBounds(50,100,200,30);
+
         frame.add(b);
         b.setBounds(50,150,200,30);
+
         b.addActionListener(this);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(null);
@@ -48,6 +50,7 @@ public class ExpenseManager implements ActionListener {
         frame.setVisible(true);
     }
     public void actionPerformed(ActionEvent e){
+
         frame.getContentPane().setBackground(Color.black);
         try{
             Class.forName("com.mysql.jdbc.Driver");
@@ -60,6 +63,7 @@ public class ExpenseManager implements ActionListener {
             String x = t1.getText();
             int x1 = Integer.parseInt(x);
             String y = t2.getText();
+
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setInt(1, x1);
             pstmt.setString(2, y);
@@ -69,6 +73,7 @@ public class ExpenseManager implements ActionListener {
 
             con.close();
             pstmt.close();
+
         }catch(Exception eX){ System.out.println(e);}
     }
 
